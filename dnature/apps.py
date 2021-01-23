4646cefe65eb3ac9430b9fd465e90a027e488459
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-01-23 07:01:44 +0700

    first commit

---
+++ dnature/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DnatureConfig(AppConfig):
+    name = 'dnature'
---
+++ dnature/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# Create your views here.
+def hello(request): #create view
+    return render(request, 'index.html', 
+    {
+        'starring':'Starring: Chris Hemsworth, Tom Hiddleston, Mark Ruffalo',
+        'creator':'Directed by Taika Waititi',
+        'background':'ttps://asset-a.grid.id/crop/0x0:0x0/x/photo/2020/09/18/47557070.jpeg',
+    }) #reference templates that i made it
+
+def europe(request):
+    return render(request,'europe.html')
